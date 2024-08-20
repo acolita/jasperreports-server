499bed2f9db98d291c4bda8c0e19ad78c38b86b5
ElthonAcolita <elthon.bicudo@acolita.com.br>
2024-08-20 12:45:06 -0300

    JasperReports Server imagem funcional

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM tomcat:9.0.65-jdk11-openjdk
 
 ENV JASPERSERVER_VERSION 8.2.0
 
-# Execute all in one layer so that it keeps the image as small as possible
 RUN wget "https://sourceforge.net/projects/jr-community-installers/files/Server/TIB_js-jrs-cp_${JASPERSERVER_VERSION}_bin.zip/download" \
          -O /tmp/jasperserver.zip  && \
     unzip /tmp/jasperserver.zip -d /usr/src/ && \
@@ -10,7 +9,7 @@ RUN wget "https://sourceforge.net/projects/jr-community-installers/files/Server/
     mv /usr/src/jasperreports-server-cp-${JASPERSERVER_VERSION}-bin /usr/src/jasperreports-server && \
     rm -r /usr/src/jasperreports-server/samples
 
-COPY resfactory.properties /usr/local/tomcat/webapps/jasperserver/WEB-INF/classes/
+VOLUME /root/
 
 ADD wait-for-it.sh /wait-for-it.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 /wait-for-it.sh $DB_HOST:$DB_PORT --timeout=60 -- echo "Database server is up"
 
 export BUILDOMATIC_MODE=script
-export ADMIN_PASSWORD='Jasper@2024!Admin'
 
 if [ -f "/.do_deploy_jasperserver" ]; then
     pushd /usr/src/jasperreports-server/buildomatic
@@ -15,17 +14,20 @@ if [ -f "/.do_deploy_jasperserver" ]; then
         
     sed -i -e "s|^dbHost.*$|dbHost=$DB_HOST|g; s|^dbPort.*$|dbPort=$DB_PORT|g; s|^dbUsername.*$|dbUsername=$DB_USER|g; s|^dbPassword.*$|dbPassword=$DB_PASSWORD|g; s|^# js.dbName=.*$|js.dbName=$DB_NAME|g" default_master.properties
 
-    sed -i -e "s|^# webAppNameCE.*$|webAppNameCE = ROOT|g" default_master.properties
+    sed -i -e "s|^# webAppNameCE.*$|webAppNameCE = $WEBAPP_NAME|g" default_master.properties
 
     echo "encrypt.done=true" >> default_master.properties
 
     sed -i -e "s|^js.dbName.*$|js.dbName=$DB_NAME|g" ./conf_source/db/${DB_TYPE}/db.template.properties
 
     ./js-ant create-js-db || true 
+    ./js-ant gen-config
     ./js-ant init-js-db-ce 
     ./js-ant import-minimal-ce 
     ./js-ant deploy-webapp-ce
 
+    echo "tbeller.usejndi=false" > $CATALINA_HOME/webapps/$WEBAPP_NAME/WEB-INF/classes/resfactory.properties
+   
     rm /.do_deploy_jasperserver
 
     popd
